69a9342f211363aad0716e99ce2a78eb7d0fbffa
hengkerrusia <anggarwantoa@gmail.com>
2026-02-23 12:59:25 +0700

    Quartz sync: Feb 23, 2026, 12:59 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,12 +4,39 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
-  afterBody: [],
+  header: [
+    Component.Flex({
+      components: [
+        { Component: Component.PageTitle() },
+        { Component: Component.Spacer() },
+        { Component: Component.Search() },
+        { Component: Component.Darkmode() },
+      ],
+    }),
+  ],
+  afterBody: [
+    Component.MobileOnly(
+      Component.Graph({
+        localGraph: {
+          drag: true,
+          zoom: true,
+          depth: 1,
+          scale: 1.1,
+          repelForce: 0.5,
+          centerForce: 0.3,
+          linkDistance: 30,
+          fontSize: 0.6,
+          opacityScale: 1,
+          showTags: true,
+          removeTags: [],
+        },
+      })
+    ),
+  ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Wargames": "https://www.pwnthe.website",
+      "Discord Community": "https://discord.gg/v9EEVKPWm2",
     },
   }),
 }
@@ -19,50 +46,120 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ConditionalRender({
       component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
+      condition: (page) => page.fileData.slug !== "index" && !page.fileData.slug?.startsWith("tags/"),
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
+    Component.DesktopOnly(
+      Component.Graph({
+        localGraph: {
+          drag: true,
+          zoom: true,
+          depth: 1,
+          scale: 1.1,
+          repelForce: 0.5,
+          centerForce: 0.3,
+          linkDistance: 30,
+          fontSize: 0.6,
+          opacityScale: 1,
+          showTags: true,
+          removeTags: [],
         },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
+        globalGraph: {
+          drag: true,
+          zoom: true,
+          depth: -1,
+          scale: 0.9,
+          repelForce: 0.5,
+          centerForce: 0.3,
+          linkDistance: 30,
+          fontSize: 0.6,
+          opacityScale: 1,
+          showTags: true,
+          removeTags: [],
+        },
+      })
+    ),
+    Component.DesktopOnly(Component.TableOfContents()),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    Component.DesktopOnly(
+      Component.Explorer({
+        title: "Notes",
+        folderClickBehavior: "link",
+        folderDefaultState: "collapsed",
+        useSavedState: true,
+        sortFn: (a, b) => {
+          if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+            return a.displayName.localeCompare(b.displayName, undefined, {
+              numeric: true,
+              sensitivity: "base",
+            })
+          }
+          if (!a.isFolder && b.isFolder) return 1
+          return -1
+        },
+        filterFn: (node) => node.slugSegment !== "tags",
+      })
+    ),
+    Component.DesktopOnly(
+      Component.RecentNotes({
+        title: "Recently Updated",
+        limit: 4,
+        showTags: false,
+        filter: (f) => f.slug !== "index" && !f.slug?.startsWith("tags/"),
+        linkToMore: false,
+      })
+    ),
+    Component.DesktopOnly(Component.Backlinks()),
   ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
-    Component.Explorer(),
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
   ],
-  right: [],
+  left: [
+    Component.MobileOnly(Component.Spacer()),
+    Component.DesktopOnly(
+      Component.Graph({
+        localGraph: {
+          drag: true,
+          zoom: true,
+          depth: 1,
+          scale: 1.1,
+          repelForce: 0.5,
+          centerForce: 0.3,
+          linkDistance: 30,
+          fontSize: 0.6,
+          opacityScale: 1,
+          showTags: true,
+        },
+      })
+    ),
+  ],
+  right: [
+    Component.DesktopOnly(
+      Component.Explorer({
+        title: "Notes",
+        folderClickBehavior: "link",
+        folderDefaultState: "collapsed",
+        useSavedState: true,
+      })
+    ),
+    Component.DesktopOnly(
+      Component.RecentNotes({
+        title: "Recently Updated",
+        limit: 5,
+        showTags: false,
+      })
+    ),
+  ],
 }
